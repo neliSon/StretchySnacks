d9d61784528725f9310a45fe28d1852ddd54f778
neliSun <nelson.chow91.nc@gmail.com>
2016-04-14 13:50:57 -0700

    implement makeStackView() and toggleStackView()

--- StretchySnacks/ViewController.swift
+++ StretchySnacks/ViewController.swift
@@ -12,13 +12,16 @@ class ViewController: UIViewController {
 
     // MARK: Properties
     var animator:UIDynamicAnimator!
+    var openBottomConstraint: NSLayoutConstraint!
+    var closedBottomConstraint: NSLayoutConstraint!
     
     @IBOutlet weak var navBarHeightConstraint: NSLayoutConstraint!
     @IBOutlet weak var plusButton: UIButton!
+    @IBOutlet weak var navBarView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         animator = UIDynamicAnimator(referenceView: view)
         makeStackView()
     }
@@ -26,8 +29,11 @@ class ViewController: UIViewController {
     // MARK: Actions
     @IBAction func plusButton(sender: UIButton) {
         print("Plus icon pressed.")
+        toggleStackView()
         animateNavBarHeight()
         rotatePlusButton()
+
+
     }
     
     // MARK: General Functions
@@ -58,7 +64,7 @@ class ViewController: UIViewController {
         let imageView4 = UIImageView(image: image4)
         let imageView5 = UIImageView(image: image5)
         
-        let imageViews:[UIImageView] = [imageView1, imageView2, imageView3, imageView4, imageView5]
+        let imageViews = [imageView1, imageView2, imageView3, imageView4, imageView5]
         
         let stackView = UIStackView(arrangedSubviews: imageViews)
         stackView.axis = .Horizontal
@@ -66,7 +72,32 @@ class ViewController: UIViewController {
         stackView.alignment = .Fill
         stackView.spacing = 5
         stackView.translatesAutoresizingMaskIntoConstraints = false
+        
         view.addSubview(stackView)
+        
+        stackView.leftAnchor.constraintEqualToAnchor(self.view.leftAnchor).active = true
+        stackView.rightAnchor.constraintEqualToAnchor(self.view.rightAnchor).active = true
+        stackView.heightAnchor.constraintEqualToConstant(navBarView.frame.height).active = true
+        
+        
+        openBottomConstraint = stackView.bottomAnchor.constraintEqualToAnchor(self.navBarView.bottomAnchor)
+        
+        closedBottomConstraint = stackView.topAnchor.constraintEqualToAnchor(self.navBarView.topAnchor, constant: -navBarView.frame.height)
+        
+        closedBottomConstraint.active = true
+        openBottomConstraint.active = false
+    }
+    
+    func toggleStackView() {
+        if navBarHeightConstraint.constant == 66.0 {
+            // going to open
+            closedBottomConstraint.active = false
+            openBottomConstraint.active = true
+        }else {
+            // otherwise will close
+            openBottomConstraint.active = false
+            closedBottomConstraint.active = true
+        }
     }
 }
 
